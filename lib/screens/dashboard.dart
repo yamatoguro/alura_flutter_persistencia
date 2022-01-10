36f824297c9045675ae88800e9b46c2abd871061
Iago Faria dos Reis <iago.dosreis@gmail.com>
2022-01-10 13:27:14 -0300

    Novas funcionalidades no Dashboard

--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -20,46 +20,74 @@ class Dashboard extends StatelessWidget {
             padding: const EdgeInsets.all(8.0),
             child: Image.asset('assets/images/bytebank_logo.png'),
           ),
-          Padding(
+          Row(
+            children: const [
+              _FeatureItem(
+                name: 'Transfer',
+                icon: Icons.monetization_on,
+              ),
+              _FeatureItem(
+                name: 'Transaction Feed',
+                icon: Icons.description,
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _FeatureItem extends StatelessWidget {
+  const _FeatureItem({
+    Key? key,
+    required this.name,
+    required this.icon,
+    this.method,
+  }) : super(key: key);
+
+  final String name;
+  final IconData icon;
+  final method;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Material(
+        color: Colors.green[900],
+        child: InkWell(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => const ContactsList(),
+              ),
+            );
+          },
+          child: Container(
             padding: const EdgeInsets.all(8.0),
-            child: Material(
-              color: Colors.green[900],
-              child: InkWell(
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => ContactsList(),
-                    ),
-                  );
-                },
-                child: Container(
-                  padding: const EdgeInsets.all(8.0),
-                  height: 100,
-                  width: 150,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: const [
-                      Icon(
-                        Icons.people,
-                        color: Colors.white,
-                        size: 24.0,
-                      ),
-                      Text(
-                        'Contacts',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 16.0,
-                        ),
-                      ),
-                    ],
+            height: 100,
+            width: 150,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Icon(
+                  icon,
+                  color: Colors.white,
+                  size: 24.0,
+                ),
+                Text(
+                  name,
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 16.0,
                   ),
                 ),
-              ),
+              ],
             ),
-          )
-        ],
+          ),
+        ),
       ),
     );
   }
