b0fd98a9d642072deb170f234467fcb8800103d5
Iago Faria dos Reis <iago.dosreis@gmail.com>
2022-01-10 20:18:37 -0300

    Simplificando as transações

--- lib/http/webclients/transaction_webclient.dart
+++ lib/http/webclients/transaction_webclient.dart
@@ -2,7 +2,6 @@ import 'dart:convert';
 
 import 'package:Bytebank/http/interceptors/logging_interceptor.dart';
 import 'package:Bytebank/http/webclient.dart';
-import 'package:Bytebank/models/contact.dart';
 import 'package:Bytebank/models/transaction.dart';
 import 'package:http/http.dart';
 import 'package:http_interceptor/http/http.dart';
@@ -16,10 +15,8 @@ class TransactionWebclient {
         .get(Uri.parse(baseURL))
         .timeout(const Duration(seconds: 5));
     final List<dynamic> decodedJson = jsonDecode(response.body);
-    final List<Transaction> transactions = [];
-    for (Map<String, dynamic> transactionJson in decodedJson) {
-      transactions.add(Transaction.fromJson(transactionJson));
-    }
+    final List<Transaction> transactions =
+        decodedJson.map((e) => Transaction.fromJson(e)).toList();
     return transactions;
   }
 
